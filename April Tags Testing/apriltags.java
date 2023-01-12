50797b024b717b4894e6027e677d4c3652d1b677
Akhil353 <asingamneni@gmail.com>
2023-01-11 18:13:02 -0800

    Added code to get targets

--- April Tags Testing/apriltags.java
+++ April Tags Testing/apriltags.java
@@ -1,3 +1,11 @@
+import java.lang.annotation.Target;
+
 public class apriltags {
-    
+    PhotonCamera camera = new PhotonCamera ("Microsoft_LifeCam_HD-3000"); //network table that photonvision sends info over
+    PhotonPipelineResult result = camera.getLatestResult(); //run this code in perodic (every 20 ms)
+    boolean hasTargets = result.hasTargets(); //check if there's a target in the result variable
+    if (hasTargets) {
+        List<PhotonTrackedTarget> targets = result.getTargets(); //if else statement for code above
+        var target = result.getBestTarget();
+    }
 }
