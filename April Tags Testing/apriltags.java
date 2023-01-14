909cf94e533ea5070138d9311925e4fa9ea8232c
Akhil353 <asingamneni@gmail.com>
2023-01-14 14:40:35 -0800

    Co-authored-by: Justin N <Jyustin@users.noreply.github.com> Co-authored-by: Advik Garg <advik.sd@gmail.com> Co-authored-by: will-w-cheng <will-w-cheng@users.noreply.github.com>

--- April Tags Testing/apriltags.java
+++ April Tags Testing/apriltags.java
@@ -2,27 +2,21 @@ import java.lang.annotation.Target;
 
 public class apriltags {
     PhotonCamera camera = new PhotonCamera ("Microsoft_LifeCam_HD-3000"); //network table that photonvision sends info over
-    PhotonPipelineResult result = camera.getLatestResult(); //run this code in perodic (every 20 ms)
-    boolean hasTargets = result.hasTargets();{ //check if there's a target in the result variable
-    if (hasTargets) {
+    Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Translation is for how much forward, left, and up from center the camera is
+    PhotonPipelineResult result = camera.getLatestResult();{ //run this code in perodic (every 20 ms)
+    if (result.hasTargets()) {
         List<PhotonTrackedTarget> targets = result.getTargets(); //if else statement for code above
-        var target = result.getBestTarget();
-    } }
+    }
+    double target_x = target.getYaw(); // Get the yaw position of target
+    double target_y = target.getPitch(); //Get pitch of target
+    double area = target.getArea(); //Get area of target
+    double skew = target.getSkew();  //Get skew of target
+    Transform3d pose = target.getCameraToTarget(); //Tells you info to go to a target
+    int targetID = target.getFiducialId(); //Get id of target
+    double distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose); //get distance to target
+    Rotation2d targetYaw = PhotonUtils.getYawToPose(robotPose, targetPose); //get yaw difference between target and robot pose
+    AprilTagFieldLayout aprilTagFieldLayout = new ApriltagFieldLayout(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile));//load map field
     
-    //THE FOLLOWING CODE IS COPY PASTED STUFF
-  /*  AprilTagFieldLayout aprilTagFieldLayout = new ApriltagFieldLayout(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2022RapidReact.m_resourceFile)); /*
-resource file should be edited to work with game, also where is the resource file?*/
-//Forward Camera
-//cam = new PhotonCamera("testCamera"); // can we use existing camera?
-//Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); /*Cam mounted facing forward, half a meter forward of center, 
-//half a meter up from center.*/
-// ... Add other cameras here
-
-// Assemble the list of cameras & mount locations
-//var camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
-//camList.add(new Pair<PhotonCamera, Transform3d>(cam, robotToCam));
-//RobotPoseEstimator robotPoseEstimator = new RobotPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camList);
 
 }
-
-
+}
