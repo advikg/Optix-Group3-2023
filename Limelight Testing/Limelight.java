2306c602d9ac2bdcdab82b1464bd2fe5405d8b4c
advikg <advik.sd@gmail.com>
2023-01-24 16:23:14 -0800

    Limelight getters

--- Limelight Testing/Limelight.java
+++ Limelight Testing/Limelight.java
@@ -4,18 +4,17 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import java.lang.annotation.Target;
 
 public class Limelight {
-    public Limelight_Getters(NetworkTableEntry tx, NetworkTableEntry ta) {
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight"); // New network table instance, and reading in the data table from limelight.
-        this.tx = tx; // Horizontal offset from crosshair to the target
-        this.ta = ta; // Target area
-
-    } 
-}
-
-public class Photongetters {
+    //Static vars
     public static PhotonCamera camera = new PhotonCamera ("Microsoft_LifeCam_HD-3000"); //network table that photonvision sends info over
     public static Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Translation is for how much forward, left, and up from center the camera is
     public static ArrayList<Pair<PhotonCamera, Transform3d>> camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
+
+    // Limelight Getters constructor class
+    public LimelightGetters(NetworkTableEntry tx, NetworkTableEntry ta) {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight"); // New network table instance, and reading in the data table from limelight.
+        this.tx = tx; // Horizontal offset from crosshair to the target
+        this.ta = ta; // Target area
+    } 
     public static void main(String[] args) {
         camList.add(new Pair<PhotonCamera, Transform3d>(camera, robotToCam));
         RobotPoseEstimator robotPoseEstimator = new RobotPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camList);
@@ -28,5 +27,5 @@ public class Photongetters {
         double area = target.getArea(); //Get area of target
         int targetID = target.getFiducialId();
     }
-    }  
+    }
 }
