9ad13f5e5075e4cde7b8968fc2134dbf47d64107
advikg <advik.sd@gmail.com>
2023-01-25 17:07:15 -0800

    Moved the getters into utils(some errors)

--- Optix-Group3-2023/src/main/java/frc/robot/utils/Limelight.java
+++ Optix-Group3-2023/src/main/java/frc/robot/utils/Limelight.java
@@ -1,12 +1,13 @@
+package frc.robot.utils;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import java.lang.annotation.Target;
 
-import javax.xml.crypto.Data;
-
-public static class Limelight {
-    private Limelight_Getters(NetworkTableEntry tx, NetworkTableEntry ta) {
+public class Limelight {
+    private static NetworkTableEntry tx;
+    private static NetworkTableEntry ta;
+    public Limelight(NetworkTableEntry tx, NetworkTableEntry ta) {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight"); // New network table instance, and reading in the data table from limelight.
         this.tx = tx; // Horizontal offset from crosshair to the target
         this.ta = ta; // Target area
---
+++ Optix-Group3-2023/src/main/java/frc/robot/utils/PhotonGetters.java
@@ -0,0 +1,43 @@
+package frc.robot.utils;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class PhotonGetters {
+    
+    static NetworkTable table = NetworkTableInstance.getDefault().getTable("photonvision"); // New network table instance, and reading in the data table from limelight.
+    
+    public enum Data{
+        x("tx"),
+        y("ty"),
+        area("ta");
+        
+        
+        public final String str;
+
+        Data(String str) {
+            this.str = str;
+        }    
+    }
+    
+
+    private static double getDouble(Data val) { // Data val just represents the values in the networking table API (tx, ty, etc)
+        return table.getEntry(val).getDouble(0.0); // Get the value based off the enum DATA (0.0 is how you read it in networking tables)
+    }
+
+    private static double getX(double tx) {
+        tx = getDouble(Data.x);
+        return tx;
+    }
+
+    private static double getY(double ty) {
+        ty = getDouble(Data.y);
+        return ty;
+    }
+     
+    private static double getArea(double area) {
+        area = getDouble(Data.Area);
+        return area;
+    }
+}
