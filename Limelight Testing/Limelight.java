00566a0f215ef75839025fac0ad13c54987e8723
will-w-cheng <will.wi.cheng@gmail.com>
2023-01-25 16:14:07 -0800

    Private instead of public (goodsir rohin)

--- Limelight Testing/Limelight.java
+++ Limelight Testing/Limelight.java
@@ -3,8 +3,8 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import java.lang.annotation.Target;
 
-public static class Limelight {
-    public Limelight_Getters(NetworkTableEntry tx, NetworkTableEntry ta) {
+private static class Limelight {
+    private Limelight_Getters(NetworkTableEntry tx, NetworkTableEntry ta) {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight"); // New network table instance, and reading in the data table from limelight.
         this.tx = tx; // Horizontal offset from crosshair to the target
         this.ta = ta; // Target area
@@ -12,18 +12,18 @@ public static class Limelight {
     } 
 }
 
-public static class Photongetters {
-    public static PhotonCamera camera = new PhotonCamera ("Microsoft_LifeCam_HD-3000"); //network table that photonvision sends info over
-    public static Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Translation is for how much forward, left, and up from center the camera is
-    public static ArrayList<Pair<PhotonCamera, Transform3d>> camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
+private static class Photongetters {
+    private static PhotonCamera camera = new PhotonCamera ("Microsoft_LifeCam_HD-3000"); //network table that photonvision sends info over
+    private static Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Translation is for how much forward, left, and up from center the camera is
+    private static ArrayList<Pair<PhotonCamera, Transform3d>> camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
 
-    public enum Dataddy{
+    private enum Dataddy{
         x("tx"),
         y("ty"),
         Area("ta");
         
         
-        public final String str;
+        private final String str;
 
         Dataddy(String str) {
             this.str = str;
@@ -36,22 +36,22 @@ public static class Photongetters {
         return table.getEntry(val).getDouble(0.0); // Get the value based off the enum DATA (0.0 is how you read it in networking tables)
     }
 
-    public static double getX() {
+    private static double getX() {
         tx = getDouble(Dataddy.x);
         return tx;
     }
 
-    public static double getY() {
+    private static double getY() {
         ty = getDouble(Dataddy.y);
         return ty;
     }
      
-    public static double getArea() {
+    private static double getArea() {
         Area = getDouble(Dataddy.Area);
         return area;
     }
     
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         camList.add(new Pair<PhotonCamera, Transform3d>(camera, robotToCam));
         RobotPoseEstimator robotPoseEstimator = new RobotPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camList);
         PhotonPipelineResult result = camera.getLatestResult();{ //run this code in perodic (every 20 ms)
